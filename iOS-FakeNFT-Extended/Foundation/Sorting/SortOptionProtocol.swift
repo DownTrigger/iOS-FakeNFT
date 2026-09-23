ed2b417ed04313c0ad5721fd34e2b62bb156fe1c
IrinaMuravyeva <miomir@yandex.ru>
2026-09-23 22:51:54 +0800

    feat(sorting): add SortOptionProtocol

---
+++ iOS-FakeNFT-Extended/Foundation/Sorting/SortOptionProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  SortOptionProtocol.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Irina Muravyeva on 23.09.2026.
+//
+
+protocol SortOptionProtocol: Identifiable, Hashable {
+    var title: SortingLocalizedText { get }
+}
