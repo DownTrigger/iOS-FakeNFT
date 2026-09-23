4da8b8ff121abf9a607931430d224b47a237b89c
IrinaMuravyeva <miomir@yandex.ru>
2026-09-23 22:54:33 +0800

    feat(sorting): add sort opltions sets for CatalogScreen, CartScreen, Profile screen, Statistic Screen

---
+++ iOS-FakeNFT-Extended/Foundation/Sorting/SortOptions/CatalogSortOption.swift
@@ -0,0 +1,24 @@
+//
+//  CatalogSortOption.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Irina Muravyeva on 23.09.2026.
+//
+
+enum CatalogSortOption: String, SortOptionProtocol {
+    case byTitle
+    case byNftCount
+    
+    var id: String {
+        rawValue
+    }
+    
+    var title: SortingLocalizedText {
+        switch self {
+        case .byTitle:
+            return .sortByTitle
+        case .byNftCount:
+            return .sortByNFTCount
+        }
+    }
+}
