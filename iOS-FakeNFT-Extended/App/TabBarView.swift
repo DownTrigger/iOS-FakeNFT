5fe90445cb8ff162fdd2d31b6660c6ab0f2ae138
Andrew Ruzavin <andrew.ruzavin@gmail.com>
2026-09-24 15:11:44 +0700

    feat(app): set up tab bar with four tabs

--- iOS-FakeNFT-Extended/App/TabBarView.swift
+++ iOS-FakeNFT-Extended/App/TabBarView.swift
@@ -3,16 +3,47 @@ import SwiftUI
 struct TabBarView: View {
     var body: some View {
         TabView {
+            ProfileView()
+                .tabItem {
+                    Label {
+                        Text(TabLocalizedText.profile.key)
+                    } icon: {
+                        Image(.icTabProfile)
+                    }
+                }
+
             CatalogSmokeView()
                 .tabItem {
                     Label {
-                        Text(NSLocalizedString("Tab.catalog", comment: ""))
+                        Text(TabLocalizedText.catalog.key)
                     } icon: {
                         Image(.icTabCatalog)
                     }
                 }
-                .backgroundStyle(.background)
+
+            CartView()
+                .tabItem {
+                    Label {
+                        Text(TabLocalizedText.cart.key)
+                    } icon: {
+                        Image(.icTabBasket)
+                    }
+                }
+
+            StatisticsView()
+                .tabItem {
+                    Label {
+                        Text(TabLocalizedText.statistics.key)
+                    } icon: {
+                        Image(.icTabStatistics)
+                    }
+                }
         }
         .tint(Color(.fnBlue))
     }
+}
+
+#Preview {
+    TabBarView()
+        .environment(ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl()))
 }
