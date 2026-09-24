ede97e688d3d101a027e82e68d791126a887d9ed
Andrew Ruzavin <andrew.ruzavin@gmail.com>
2026-09-25 06:42:04 +0700

    feat(catalog): add sortBy parameter to collections request

--- iOS-FakeNFT-Extended/Services/Requests/CollectionsRequest.swift
+++ iOS-FakeNFT-Extended/Services/Requests/CollectionsRequest.swift
@@ -3,13 +3,18 @@ import Foundation
 struct CollectionsRequest: NetworkRequest {
     let page: Int
     let size: Int
+    var sortBy: String?
 
     var endpoint: URL? {
         var components = URLComponents(string: "\(RequestConstants.baseURL)/api/v1/collections")
-        components?.queryItems = [
+        var queryItems = [
             URLQueryItem(name: "page", value: String(page)),
             URLQueryItem(name: "size", value: String(size))
         ]
+        if let sortBy {
+            queryItems.append(URLQueryItem(name: "sortBy", value: sortBy))
+        }
+        components?.queryItems = queryItems
         return components?.url
     }
 }
