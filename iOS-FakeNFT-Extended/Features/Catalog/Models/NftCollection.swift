eb80b974720149378f5a8e7dc9dcba305577db36
Andrew Ruzavin <andrew.ruzavin@gmail.com>
2026-09-25 02:25:44 +0700

    feat(catalog): add cover and NFT count to collection model

--- iOS-FakeNFT-Extended/Features/Catalog/Models/NftCollection.swift
+++ iOS-FakeNFT-Extended/Features/Catalog/Models/NftCollection.swift
@@ -3,4 +3,6 @@ import Foundation
 struct NftCollection: Identifiable, Sendable {
     let id: String
     let name: String
+    let cover: URL?
+    let nftCount: Int
 }
--- iOS-FakeNFT-Extended/Features/Catalog/ViewModels/CatalogViewModel.swift
+++ iOS-FakeNFT-Extended/Features/Catalog/ViewModels/CatalogViewModel.swift
@@ -4,8 +4,23 @@ import Foundation
 @Observable
 final class CatalogViewModel {
     private(set) var collections: [NftCollection] = [
-        NftCollection(id: "1", name: "Peach"),
-        NftCollection(id: "2", name: "Blue"),
-        NftCollection(id: "3", name: "Brown")
+        NftCollection(
+            id: "1",
+            name: "Peach",
+            cover: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/Peach.png"),
+            nftCount: 11
+        ),
+        NftCollection(
+            id: "2",
+            name: "Blue",
+            cover: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/Blue.png"),
+            nftCount: 6
+        ),
+        NftCollection(
+            id: "3",
+            name: "Brown",
+            cover: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/Brown.png"),
+            nftCount: 8
+        )
     ]
 }
