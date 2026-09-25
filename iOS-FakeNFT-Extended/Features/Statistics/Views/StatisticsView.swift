36560532f72405057bdcaa808f4353d3e70abf33
IrinaMuravyeva <miomir@yandex.ru>
2026-09-26 00:11:00 +0800

    feat(UserStatisticDetailView): add link from StatisticsView to UserStatisticDetailView(after touch on user)

--- iOS-FakeNFT-Extended/Features/Statistics/Views/StatisticsView.swift
+++ iOS-FakeNFT-Extended/Features/Statistics/Views/StatisticsView.swift
@@ -9,18 +9,25 @@ struct StatisticsView: View {
     }
     
     var body: some View {
-        ZStack(alignment: .bottom) {
+//        ZStack(alignment: .bottom) {
             ScrollView {
                 LazyVStack(spacing: 8) {
                     ForEach(viewModel.statistics) { statistic in
-                        UserStatisticView(
-                            position: statistic.position,
-                            name: statistic.user.username,
-                            avatar: Image(
-                                statistic.user.avatar ?? "imgAvatarPlaceholder"
-                            ),
-                            countNft: statistic.countNft
-                        )
+                        NavigationLink {
+                            UserStatisticDetailView(
+                                user: statistic.user
+                            )
+                        } label: {
+                            UserStatisticView(
+                                position: statistic.position,
+                                name: statistic.user.username,
+                                avatar: Image(
+                                    statistic.user.avatar ?? "imgAvatarPlaceholder"
+                                ),
+                                countNft: statistic.countNft
+                            )
+                        }
+                        .buttonStyle(.plain)
                     }
                 }
                 .padding(.horizontal, 16)
@@ -28,40 +35,55 @@ struct StatisticsView: View {
                 .padding(.bottom, 8)
             }
             .background(.background)
-            
-            if isSortSheetPresented {
-                Color(.fnOverlay)
-                    .ignoresSafeArea()
-                    .onTapGesture {
-                        isSortSheetPresented = false
+            .sortSheet(
+                isPresented: $isSortSheetPresented,
+                options: [
+                    StatisticsSortOption.byName,
+                    StatisticsSortOption.byRating
+                ],
+                onSelect: { option in
+                    switch option {
+                    case .byName:
+                        viewModel.sortStatisticsByName()
+
+                    case .byRating:
+                        viewModel.sortStatisticsByRating()
                     }
-                
-                SortBottomSheet(
-                    screenTitle: .sorting,
-                    options: [
-                        StatisticsSortOption.byName,
-                        StatisticsSortOption.byRating
-                    ],
-                    onSelect: { option in
-                        switch option {
-                        case .byName:
-                            viewModel.sortStatisticsByName()
-                            
-                        case .byRating:
-                            viewModel.sortStatisticsByRating()
-                        }
-                        
-                        withAnimation {
-                            isSortSheetPresented = false
-                        }
-                    },
-                    onClose: {
-                        isSortSheetPresented = false
-                    }
-                )
-                .transition(.move(edge: .bottom))
-            }
-        }
+                }
+            )
+//            if isSortSheetPresented {
+//                Color(.fnOverlay)
+//                    .ignoresSafeArea()
+//                    .onTapGesture {
+//                        isSortSheetPresented = false
+//                    }
+//                
+//                SortBottomSheet(
+//                    screenTitle: .sorting,
+//                    options: [
+//                        StatisticsSortOption.byName,
+//                        StatisticsSortOption.byRating
+//                    ],
+//                    onSelect: { option in
+//                        switch option {
+//                        case .byName:
+//                            viewModel.sortStatisticsByName()
+//                            
+//                        case .byRating:
+//                            viewModel.sortStatisticsByRating()
+//                        }
+//                        
+//                        withAnimation {
+//                            isSortSheetPresented = false
+//                        }
+//                    },
+//                    onClose: {
+//                        isSortSheetPresented = false
+//                    }
+//                )
+//                .transition(.move(edge: .bottom))
+//            }
+//        }
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 NavigationSortButton {
