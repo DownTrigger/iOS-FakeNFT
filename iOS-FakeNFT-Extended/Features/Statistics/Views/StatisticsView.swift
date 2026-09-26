287fdd28757cfe271021ea6c249add443a8e2c39
IrinaMuravyeva <miomir@yandex.ru>
2026-09-26 11:47:54 +0800

    refactor(StatisticsView): refactor after simplify SortSheetModifier

--- iOS-FakeNFT-Extended/Features/Statistics/Views/StatisticsView.swift
+++ iOS-FakeNFT-Extended/Features/Statistics/Views/StatisticsView.swift
@@ -9,81 +9,47 @@ struct StatisticsView: View {
     }
     
     var body: some View {
-//        ZStack(alignment: .bottom) {
-            ScrollView {
-                LazyVStack(spacing: 8) {
-                    ForEach(viewModel.statistics) { statistic in
-                        NavigationLink {
-                            UserStatisticDetailView(
-                                user: statistic.user
-                            )
-                        } label: {
-                            UserStatisticView(
-                                position: statistic.position,
-                                name: statistic.user.username,
-                                avatar: Image(
-                                    statistic.user.avatar ?? "imgAvatarPlaceholder"
-                                ),
-                                countNft: statistic.countNft
-                            )
-                        }
-                        .buttonStyle(.plain)
+        ScrollView {
+            LazyVStack(spacing: 8) {
+                ForEach(viewModel.statistics) { statistic in
+                    NavigationLink {
+                        UserStatisticDetailView(
+                            user: statistic.user
+                        )
+                    } label: {
+                        UserStatisticView(
+                            position: statistic.position,
+                            name: statistic.user.username,
+                            avatar: Image(
+                                statistic.user.avatar ?? "imgAvatarPlaceholder"
+                            ),
+                            countNft: statistic.countNft
+                        )
                     }
+                    .buttonStyle(.plain)
                 }
-                .padding(.horizontal, 16)
-                .padding(.top, 20)
-                .padding(.bottom, 8)
             }
-            .background(.background)
-            .sortSheet(
-                isPresented: $isSortSheetPresented,
-                options: [
-                    StatisticsSortOption.byName,
-                    StatisticsSortOption.byRating
-                ],
-                onSelect: { option in
-                    switch option {
-                    case .byName:
-                        viewModel.sortStatisticsByName()
-
-                    case .byRating:
-                        viewModel.sortStatisticsByRating()
-                    }
+            .padding(.horizontal, 16)
+            .padding(.top, 20)
+            .padding(.bottom, 8)
+        }
+        .background(.background)
+        .sortSheet(
+            isPresented: $isSortSheetPresented,
+            options: [
+                StatisticsSortOption.byName,
+                StatisticsSortOption.byRating
+            ],
+            onSelect: { option in
+                switch option {
+                case .byName:
+                    viewModel.sortStatisticsByName()
+                    
+                case .byRating:
+                    viewModel.sortStatisticsByRating()
                 }
-            )
-//            if isSortSheetPresented {
-//                Color(.fnOverlay)
-//                    .ignoresSafeArea()
-//                    .onTapGesture {
-//                        isSortSheetPresented = false
-//                    }
-//                
-//                SortBottomSheet(
-//                    screenTitle: .sorting,
-//                    options: [
-//                        StatisticsSortOption.byName,
-//                        StatisticsSortOption.byRating
-//                    ],
-//                    onSelect: { option in
-//                        switch option {
-//                        case .byName:
-//                            viewModel.sortStatisticsByName()
-//                            
-//                        case .byRating:
-//                            viewModel.sortStatisticsByRating()
-//                        }
-//                        
-//                        withAnimation {
-//                            isSortSheetPresented = false
-//                        }
-//                    },
-//                    onClose: {
-//                        isSortSheetPresented = false
-//                    }
-//                )
-//                .transition(.move(edge: .bottom))
-//            }
-//        }
+            }
+        )
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 NavigationSortButton {
